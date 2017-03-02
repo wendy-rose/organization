6762b6e9eb129f0c821ba7d0724ed5ef43948e80
wendy-rose <1104777947@qq.com>
2017-03-02 08:47:26 +0800

    测试

--- public/static/user/js/user.js
+++ public/static/user/js/user.js
@@ -1,13 +1,8 @@
 $(function(){
 	$('#avatar').on('change', function(){
 		$('#upload').ajaxSubmit(function(data){
-            console.log('/uploads/' + data.data.url);
             $('#test').attr('url', data.data.url);
-			$("input[name='avatar_url']").val('/uploads/' + data.data.url);
-			img = $('<img scr="' +"__ROOT__" + '/uploads/' + data.data.url + '">');
-			$('.avatar-wrapper').empty().html(img);
-			$('.avatar-preview').empty().html(img);
-			img.cropper({
+			$('#test').cropper({
 				spectRatio: 1,
 				preview: $('.avatar-preview').selector,
 				crop: function(e){
