b506bceabf343458ab65195ae3028e607eeb27bb
wendy-rose <1104777947@qq.com>
2017-05-09 00:02:34 +0800

    debug

---
+++ public/static/activity/js/add.js
@@ -0,0 +1,25 @@
+$(function(){
+	var ueOption = {
+        serverUrl: '/index/index/ue',
+        toolbars: [
+            ['source', 'undo', 'redo', '|', 'fontfamily', 'fontsize', 'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', 'justifyleft', 'justifyright', 'justifycenter', 'justifyjustify', 'imagecenter', 'lineheight', 'rowspacingtop', 'rowspacingbottom', 'paragraph', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'time', 'date', 'simpleupload', 'insertimage', 'cleardoc']
+        ],
+        autoHeightEnabled: true,
+        autoFloatEnabled: true,
+        allowDivTransToP: false,
+        initialContent: '活动详细内容'
+    };
+    var ue = UE.getEditor('content', ueOption);
+
+    $('#openapply').click(function(){
+    	alert(1);
+    	var checked = $(this).is(':checked');
+    	if (checked) {
+    		$('#begin').attr("disabled", false);
+    		$('#end').attr("disabled", false);
+    	}else {
+    		$('#begin').attr("disabled", true);
+    		$('#end').attr("disabled", true);
+    	}
+    });
+});
